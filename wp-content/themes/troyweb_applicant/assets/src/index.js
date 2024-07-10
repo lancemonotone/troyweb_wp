2d244afa6a32babf02ad46486c9abcb879499f5b
Rus Miller <lancemonotone@gmail.com>
2024-07-10 08:36:55 -0400

    Moved components into components.js.

--- wp-content/themes/troyweb_applicant/assets/src/index.js
+++ wp-content/themes/troyweb_applicant/assets/src/index.js
@@ -1,13 +1,5 @@
 /**
  * Theme Styles
- *
- * See vite.config.js for global scss styles.
- * Includes the following:
- * '../src/scss/utility/_normalize.scss';
- * '../src/scss/utility/_a11y.scss';
- * '../src/scss/utility/_theme.scss';
- * '../src/scss/utility/_print.scss';
- * '../src/scss/utility/_typography.scss';
  */
 
 // Import all of Bootstrap's JS
@@ -17,22 +9,7 @@ import * as bootstrap from 'bootstrap'
 import './index.scss'
 
 /** Components */
-// Add class to populated inputs
-import './components/select-populated-form-fields/select-populated-form-fields.js'
-
-// Smooth scroll
-import './components/smooth-scroll/smooth-scroll.js'
-
-// Primary navigation
-// import './components/primary-navigation/primary-navigation.js'
-
-// Expando
-// import './components/expando/expando.js'
-
-// Accordion
-// import './components/accordion/accordion.js'
-
-// Click to copy
-import './components/click-to-copy/click-to-copy.js'
+import './components/components.js'
 
 /**  Layouts */
+import './layouts/layouts.js'
