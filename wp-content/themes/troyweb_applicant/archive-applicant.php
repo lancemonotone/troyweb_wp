139e842c5d0329e6cc98967da0a79111b94c7b23
Rus Miller <lancemonotone@gmail.com>
2024-07-13 11:11:11 -0400

    Applicant: Added single and archive templates.

---
+++ wp-content/themes/troyweb_applicant/archive-applicant.php
@@ -0,0 +1,23 @@
+<?php namespace monotone;
+
+get_header();
+?>
+    <?php if ( have_posts() ) { ?>
+
+    <?php the_archive_title( '<h1>', '</h1>' ); ?>
+
+    <?php if ( $description = get_the_archive_description() ) { ?>
+        <div class="archive-description"><?= wp_kses_post( wpautop( $description ) ) ?></div>
+    <?php } ?>
+
+    <?php get_template_part( 'parts/blog_posts' ); ?>
+
+<?php } else { ?>
+
+    <p><?php
+        /* translators: Nothing found text. */
+        esc_html_e( 'No posts found.', 'monotone' ); ?></p>
+
+<?php } ?>
+
+    <?php get_footer();
---
+++ wp-content/themes/troyweb_applicant/single-applicant.php
@@ -0,0 +1,8 @@
+<?php namespace monotone;
+
+get_header(); ?>
+
+<h1><?php the_title(); ?></h1>
+<p><?php the_content(); ?></p>
+
+<?php get_footer(); ?>
